7f423aad73e2b3326a2535cdcf9438ea53c50d02
Levi-Opunga <leviopunga@gmail.com>
2022-05-31 01:36:31 +0300

    added dalogue fragment toggler

--- app/src/main/java/com/moringaschool/weekone/MainActivity.java
+++ app/src/main/java/com/moringaschool/weekone/MainActivity.java
@@ -2,6 +2,8 @@ package com.moringaschool.weekone;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,33 +11,51 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.moringaschool.weekone.databinding.ActivityMainBinding;
+import com.moringaschool.weekone.databinding.ActivitySecondBinding;
 import com.moringaschool.weekone.fragments.BlankFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-@BindView(R.id.send)
-Button sendButton;
-@BindView(R.id.disp1)
-TextView disp1;
-@BindView(R.id.input3)
-public EditText input;
-
+    private ActivityMainBinding binding;
+//@BindView(R.id.send)
+//Button sendButton;
+//@BindView(R.id.disp1)
+//TextView disp1;
+//@BindView(R.id.input3)
+//public EditText input;
 public static String A;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        sendButton.setOnClickListener((view)-> {
+//        ButterKnife.bind(this);
 
-            A = input.getText().toString();
+
+        String that = "Your message to the desert was:";
+        if(ShareData.clicked&& ShareData.getPass().length()>1){
+            binding.textView6.setText(that);
+        }else{
+            binding.textView6.setText("    ");
+        }
+
+        binding.disp1.setText(ShareData.getPass());
+
+
+        binding.send.setOnClickListener((view)-> {
+           String message = binding.input3.getText().toString();
+            ShareData share = new ShareData();
+            share.setPass(message);
             BlankFragment frag = new BlankFragment();
             frag.show(getSupportFragmentManager(),"selection");
-
         });
+
 
     }
 }
